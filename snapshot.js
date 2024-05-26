ed8e5c2c05ba76ea2ee208c934ed26c0aed69298
den330 <wo131485508@qq.com>
2024-05-26 17:16:11 -0400

    update

--- snapshot.js
+++ snapshot.js
@@ -1,1 +1,10 @@
-console.log("snapshot.js loaded");
+import puppeteer from "puppeteer";
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("http://localhost:3000");
+  await page.setViewport({ width: 1080, height: 1024 });
+  const image = await page.screenshot({ path: "/public/screenshot.png" });
+  await browser.close();
+})();
